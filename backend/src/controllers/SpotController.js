f8958ac075da8353dec43288316902a9563a1ac1
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2019-10-02 17:06:28 -0300

    Primeiro Commit do projeto Omnistack

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,11 @@
+const spot = require('../models/Spot');
+
+
+module.exports = {
+    async store(req, res) {
+        console.log(req.body);
+        console.log(req.file);
+
+        return res.json({ ok: true })
+    }
+};
