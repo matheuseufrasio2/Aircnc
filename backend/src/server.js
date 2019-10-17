2fd409ff1dff57384f48b4f2b82eb00dcf7a3d49
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2019-10-17 19:43:40 -0300

    terminando a parte de front end da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb+srv://matheus:matheus@omnistack-ygnwm.mongodb.net/sema
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
